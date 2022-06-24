4384ee9f8d7bd72c622d5dfed9f7ab09ec1cf420
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-24 08:08:05 -0600

    Half inverted pyramid in C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Half Inverted Numeric Pyramid");
+
+Console.WriteLine("Enter a Number of Rows");
+
+int number = Int32.Parse(Console.ReadLine() ?? "n");
+
+Console.WriteLine("Final Pyramid:");
+
+for (int i = number; i >= 1; i--)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+Console.WriteLine("End Program");
